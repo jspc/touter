452d5bfa80cc3d84c92cabcf22a9c4e37a54080b
jspc <james@zero-internet.org.uk>
2014-07-06 14:23:15 +0100

    Be a little more intelligent with state output

--- src/touter.go
+++ src/touter.go
@@ -16,23 +16,24 @@ var config string
 
 func init(){
     flag.StringVar(&server, "server", "localhost", "Server to send to")
-    flag.StringVar(&server, "s", "localhost", "Server to send to")
+    flag.StringVar(&server, "s", "localhost", "Server to send to (Shorthand)")
 
     flag.IntVar(&port, "port", 2002, "Port to connect on")
-    flag.IntVar(&port, "p", 2002, "Port to connect on")
+    flag.IntVar(&port, "p", 2002, "Port to connect on (Shorthand)")
 
     flag.StringVar(&config, "file", "/etc/touter.ini", "Config file for touter")
-    flag.StringVar(&config, "f", "/etc/touter.ini", "Config file for touter")
+    flag.StringVar(&config, "f", "/etc/touter.ini", "Config file for touter (Shorthand)")
 }
 
-func show_config(){
-    fmt.Println(os.Args[0], " running as per:")
-    fmt.Println("Server: ", server)
-    fmt.Println("Port: ", port)
-    fmt.Println("Path: ", config)
+func show_config() string{
+    return fmt.Sprintf("%s running with:\n\tServer: %s\n\tPort: %d\n\tConfig: %s",
+        os.Args[0],
+        server,
+        port,
+        config)
 }
 
 func main(){
     flag.Parse()
-    show_config()
+    fmt.Println( show_config() )
 }
