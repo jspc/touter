05692374cd7a4535badda176de5f46f9409cf214
jspc <james@zero-internet.org.uk>
2014-07-09 17:39:29 +0100

    Simple test server written in ruby

---
+++ src/test_server.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+#
+# Very, very simple UDP listener and JSON parser
+
+require 'socket'
+require 'json'
+
+s = UDPSocket.new
+s.bind(nil, 2002)
+
+j, sender = s.recvfrom(10240)
+data = JSON.parse j
+
+puts "Received data about #{data.keys.first}"
+puts "Sample repo: #{data[data.keys.first].first['Path'] }"
